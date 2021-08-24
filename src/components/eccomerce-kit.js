fced03f5b4d7eb5527c2e6943cf1b38a5535e669
tahaOsama <taha.alphasquad@gmail.com>
2021-08-24 08:42:06 -0700

    eccomerce-page-added

--- src/components/eccomerce-kit.js
+++ src/components/eccomerce-kit.js
@@ -244,7 +244,7 @@ function App() {
     <!-- Naviagtion -->
     <!-- hero II -->
 
-    <div class="dark:bg-gray-900">
+    <div class="dark:bg-gray-900 lg:px-0 px-4">
       <div class="container mx-auto py-9 md:py-12 lg:py-24">
         <div class="flex flex-col lg:flex-row justify-center items-strech">
           <div class="lg:w-4/12 flex justify-center items-center">
@@ -423,55 +423,55 @@ function App() {
     <!-- Best Seller 1-->
     <!-- CTA IV-->
 
-      <div class="mx-auto container flex flex-col lg:flex-row justify-center items-center lg:space-x-8 space-y-6 lg:space-y-0">
-        <div class="relative w-1/2">
-          <img class="hidden lg:block w-full" src="https://i.ibb.co/XXKZ7Pt/Group-1785.png" alt="table-chair" />
-          <img class="hidden sm:block lg:hidden" src="https://i.ibb.co/KK7Vn9n/Group-1785-1.png" alt="table-chair" />
-          <img class="sm:hidden" src="https://i.ibb.co/F0T98KC/Group-1785.png" alt="table-chair" />
-          <div class="absolute bottom-4 sm:bottom-10 inset-x-4 sm:inset-x-10 p-6 bg-white flex flex-col justify-start items-start">
-            <div>
-              <p class="text-2xl font-semibold leading-6 text-gray-800">Kitchen Catalog</p>
-            </div>
-            <div class="mt-2">
-              <p class="text-base leading-6 sm:leading-4 text-gray-600">Explore kitchen’s latest interior designs</p>
-            </div>
-            <div class="mt-6">
-              <div class="flex justify-between items-center space-x-2">
-                <a class="text-base font-medium leading-none hover:underline text-gray-800" href="javascript:void(0)">Explore</a>
-                <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M3.33337 8H12.6667" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
-                  <path d="M10 10.6667L12.6667 8" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
-                  <path d="M10 5.33333L12.6667 8" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-              </div>
-            </div>
+    <div class="mx-auto container flex flex-col lg:flex-row justify-center items-center lg:space-x-8 lg:px-0 px-4">
+      <div class="relative lg:w-1/2 w-full">
+        <img class="hidden lg:block w-full" src="https://i.ibb.co/XXKZ7Pt/Group-1785.png" alt="table-chair" />
+        <img class="hidden sm:block lg:hidden w-full" src="https://i.ibb.co/KK7Vn9n/Group-1785-1.png" alt="table-chair" />
+        <img class="sm:hidden w-full" src="https://i.ibb.co/F0T98KC/Group-1785.png" alt="table-chair" />
+        <div class="absolute bottom-4 sm:bottom-10 inset-x-4 sm:inset-x-10 p-6 bg-white flex flex-col justify-start items-start">
+          <div>
+            <p class="text-2xl font-semibold leading-6 text-gray-800">Kitchen Catalog</p>
           </div>
-        </div>
-
-        <div class="relative w-1/2">
-          <img class="hidden lg:block w-full" src="https://i.ibb.co/0qHj658/Group-1786.png" alt="chair-wood-fire" />
-          <img class="hidden sm:block lg:hidden" src="https://i.ibb.co/mGM3xtB/Group-1786-1.png" alt="chair-wood-fire" />
-          <img class="sm:hidden" src="https://i.ibb.co/ZVX8rPm/Group-1786.png" alt="chair-wood-fire" />
-          <div class="absolute bottom-4 sm:bottom-10 inset-x-4 sm:inset-x-10 p-6 bg-white flex flex-col justify-start items-start">
-            <div>
-              <p class="text-2xl font-semibold leading-6 text-gray-800">Patio Catalog</p>
-            </div>
-            <div class="mt-2">
-              <p class="text-base leading-6 sm:leading-4 text-gray-600">Explore Patio’s latest interior design collection</p>
-            </div>
-            <div class="mt-6">
-              <div class="flex justify-between items-center space-x-2">
-                <a class="text-base font-medium leading-none hover:underline text-gray-800" href="javascript:void(0)">Explore</a>
-                <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M3.33337 8H12.6667" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
-                  <path d="M10 10.6667L12.6667 8" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
-                  <path d="M10 5.33333L12.6667 8" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-              </div>
+          <div class="mt-2">
+            <p class="text-base leading-6 sm:leading-4 text-gray-600">Explore kitchen’s latest interior designs</p>
+          </div>
+          <div class="mt-6">
+            <div class="flex justify-between items-center space-x-2">
+              <a class="text-base font-medium leading-none hover:underline text-gray-800" href="javascript:void(0)">Explore</a>
+              <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3.33337 8H12.6667" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M10 10.6667L12.6667 8" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M10 5.33333L12.6667 8" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
             </div>
           </div>
         </div>
       </div>
+
+      <div class="relative lg:w-1/2 w-full lg:mt-0 mt-8">
+        <img class="hidden lg:block w-full" src="https://i.ibb.co/0qHj658/Group-1786.png" alt="chair-wood-fire" />
+        <img class="hidden sm:block lg:hidden w-full" src="https://i.ibb.co/mGM3xtB/Group-1786-1.png" alt="chair-wood-fire" />
+        <img class="sm:hidden w-full" src="https://i.ibb.co/ZVX8rPm/Group-1786.png" alt="chair-wood-fire" />
+        <div class="absolute bottom-4 sm:bottom-10 inset-x-4 sm:inset-x-10 p-6 bg-white flex flex-col justify-start items-start">
+          <div>
+            <p class="text-2xl font-semibold leading-6 text-gray-800">Patio Catalog</p>
+          </div>
+          <div class="mt-2">
+            <p class="text-base leading-6 sm:leading-4 text-gray-600">Explore Patio’s latest interior design collection</p>
+          </div>
+          <div class="mt-6">
+            <div class="flex justify-between items-center space-x-2">
+              <a class="text-base font-medium leading-none hover:underline text-gray-800" href="javascript:void(0)">Explore</a>
+              <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3.33337 8H12.6667" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M10 10.6667L12.6667 8" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M10 5.33333L12.6667 8" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <!-- CTA IV-->
     <!-- CTA II-->
